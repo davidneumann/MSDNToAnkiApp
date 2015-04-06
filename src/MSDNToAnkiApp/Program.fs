be88e12ac83bef365bf180fc3814e2a7319f83aa
David Neumann <Kazurik@gmail.com>
2015-04-06 19:27:29 -0400

    Filled in parseNamespace

--- src/MSDNToAnkiApp/Program.fs
+++ src/MSDNToAnkiApp/Program.fs
@@ -1,10 +1,10 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-
-let parseLibrary url =
-  failwith "Not implemented"
+﻿open System
+open HtmlAgilityPack
 
 let parseNamespace url =
+  let web = new HtmlWeb()
+  let doc = web.Load(url)
+
   failwith "Not implemented"
 
 let parseModule url =
@@ -16,8 +16,14 @@ let parseType url =
 let parseTypeAbbreviation url =
   failwith "Not implemented"
 
+let parseLibrary (url:string) =
+  let web = new HtmlWeb()
+  let doc = web.Load(url)
+  let namespaces = doc.DocumentNode.SelectNodes("//a[@id='sectionToggle1']/../div//tr//td[1]//a")
+                    |> Seq.map (fun a -> a.Attributes.["href"].Value)
+  namespaces |> Seq.iter parseNamespace
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
-
+  parseLibrary "https://msdn.microsoft.com/en-us/library/ee353567.aspx"
+  0
