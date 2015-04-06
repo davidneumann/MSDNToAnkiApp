ae37224354d57c0ecda0eef269c63a353b41f28d
David Neumann <Kazurik@gmail.com>
2015-04-06 18:44:24 -0400

    Added in placeholder functions

--- src/MSDNToAnkiApp/Program.fs
+++ src/MSDNToAnkiApp/Program.fs
@@ -1,6 +1,21 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+let parseLibrary url =
+  failwith "Not implemented"
+
+let parseNamespace url =
+  failwith "Not implemented"
+
+let parseModule url =
+  failwith "Not implemented"
+
+let parseType url =
+  failwith "Not implemented"
+
+let parseTypeAbbreviation url =
+  failwith "Not implemented"
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
