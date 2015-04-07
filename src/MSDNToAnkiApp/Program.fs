486d5131de0bf0a7ab09c3aa43e6c38cf6502719
David Neumann <Kazurik@gmail.com>
2015-04-06 21:12:56 -0400

    Implemented a basic parseType. Still needs to extract cards

--- src/MSDNToAnkiApp/Program.fs
+++ src/MSDNToAnkiApp/Program.fs
@@ -12,16 +12,31 @@ let trySelectNodes node xpath =
 
 let extractTableInfo (headings:HtmlNodeCollection) targetHeading =
   if headings |> Seq.exists (fun h -> h.InnerText.Trim() = targetHeading) then
-    headings |> Seq.find (fun h -> h.InnerText.Trim() = targetHeading) |> (fun h -> selectNodes h "..//tr//td[1]//a") |> Seq.map (fun n -> n.Attributes.["href"].Value)
+    headings |> Seq.find (fun h -> h.InnerText.Trim() = targetHeading) 
+      |> (fun h -> selectNodes h "..//tr//td[1]//a") |> Seq.map (fun n -> n.Attributes.["href"].Value)
   else Seq.empty
+
+let extractTableInfo2Column (headings:HtmlNodeCollection) targetHeading =
+  if headings |> Seq.exists (fun h -> h.InnerText.Trim() = targetHeading) then
+    let firstColumn = headings |> Seq.find (fun h -> h.InnerText.Trim() = targetHeading) 
+                        |> (fun h -> selectNodes h "..//tr//td[1]//a") |> Seq.map (fun n -> n.Attributes.["href"].Value)
+    let secondColumn = headings |> Seq.find (fun h -> h.InnerText.Trim() = targetHeading) 
+                        |> (fun h -> selectNodes h "..//tr//td[2]//a") |> Seq.map (fun n -> n.Attributes.["href"].Value)
+    Seq.zip firstColumn secondColumn
+  else Seq.empty
+
+let getHeadings url =
+  let web = new HtmlWeb()
+  let doc = web.Load(url)
+  selectNodes doc.DocumentNode "//h2[@class='LW_CollapsibleArea_TitleDiv']"
 
 let parseValue url =
   let web = new HtmlWeb()
   let doc = web.Load(url)
   let name = htmlDecode (doc.DocumentNode.SelectSingleNode("//h1[@class='title']").InnerText.Trim())
   let description = htmlDecode (doc.DocumentNode.SelectSingleNode("//div[@class='introduction']/p[1]").InnerText.Trim())
-
-  printfn "%s\n%s\n" name description
+  
+  ()
   //todo: Extract the screenshot of any example code
   //todo: Export these to cards
 
@@ -29,18 +44,26 @@ let parseTypeAbbreviation url =
   failwith "Not implemented"
 
 let parseType url =
-  failwith "Not implemented"
+  let web = new HtmlWeb()
+  let doc = web.Load(url)
+  let name = htmlDecode (doc.DocumentNode.SelectSingleNode("//h1[@class='title']").InnerText.Trim())
+  let description = htmlDecode (doc.DocumentNode.SelectSingleNode("//div[@class='introduction']/p[1]").InnerText.Trim())
+  let headings = selectNodes doc.DocumentNode "//h2[@class='LW_CollapsibleArea_TitleDiv']"
+  
+  let instanceMembers = extractTableInfo2Column headings "Instance Members"
+  let staticMembers = extractTableInfo2Column headings "Static Members"
+  let unionCases = extractTableInfo2Column headings "Union Cases"
+  
+  ()
+  //todo: Export these to cards
 
 let parseModule url =
-  let web = new HtmlWeb()
-  let doc = web.Load(url)
-  let headings = selectNodes doc.DocumentNode "//h2[@class='LW_CollapsibleArea_TitleDiv']"
+  let headings = getHeadings url
   extractTableInfo headings "Values" |> Seq.iter parseValue
+  //todo: Extract screenshot of any example code (see https://msdn.microsoft.com/en-us/library/ee353880.aspx)
 
 let parseNamespace url =
-  let web = new HtmlWeb()
-  let doc = web.Load(url)
-  let headings = selectNodes doc.DocumentNode "//h2[@class='LW_CollapsibleArea_TitleDiv']"
+  let headings = getHeadings url
   
   extractTableInfo headings "Modules" |> Seq.iter parseModule
   extractTableInfo headings "Type Definitions" |> Seq.iter parseType
