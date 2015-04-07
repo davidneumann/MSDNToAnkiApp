101c511f3d043546273b416d35a2463a57f5750e
David Neumann <Kazurik@gmail.com>
2015-04-06 21:38:01 -0400

    Moved the helper functions to their own file.

--- src/MSDNToAnkiApp/Program.fs
+++ src/MSDNToAnkiApp/Program.fs
@@ -1,31 +1,7 @@
 ﻿open System
 open HtmlAgilityPack
 
-let htmlDecode str = System.Net.WebUtility.HtmlDecode(str)
-
-let selectNodes (node:HtmlNode) (xpath:string) = node.SelectNodes(xpath)
-
-let trySelectNodes node xpath = try Some(selectNodes node xpath) with | _ -> None
-
-let extractTableInfo (headings:HtmlNodeCollection) targetHeading =
-  if headings |> Seq.exists (fun h -> h.InnerText.Trim() = targetHeading) then
-    headings |> Seq.find (fun h -> h.InnerText.Trim() = targetHeading) 
-      |> (fun h -> selectNodes h "..//tr//td[1]//a") |> Seq.map (fun n -> n.Attributes.["href"].Value)
-  else Seq.empty
-
-let extractTableInfo2Column (headings:HtmlNodeCollection) targetHeading =
-  if headings |> Seq.exists (fun h -> h.InnerText.Trim() = targetHeading) then
-    let firstColumn = headings |> Seq.find (fun h -> h.InnerText.Trim() = targetHeading) 
-                        |> (fun h -> selectNodes h "..//tr//td[1]//a") |> Seq.map (fun n -> n.Attributes.["href"].Value)
-    let secondColumn = headings |> Seq.find (fun h -> h.InnerText.Trim() = targetHeading) 
-                        |> (fun h -> selectNodes h "..//tr//td[2]//a") |> Seq.map (fun n -> n.Attributes.["href"].Value)
-    Seq.zip firstColumn secondColumn
-  else Seq.empty
-
-let getHeadings url =
-  let web = new HtmlWeb()
-  let doc = web.Load(url)
-  selectNodes doc.DocumentNode "//h2[@class='LW_CollapsibleArea_TitleDiv']"
+open ExtractorHelpers
 
 let parseValue url =
   let web = new HtmlWeb()
