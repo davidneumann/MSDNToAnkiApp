7df58ba7a2f4908b2dd48921dfa6514fa86de861
David Neumann <Kazurik@gmail.com>
2015-04-06 21:52:58 -0400

    Added full extraction of parseType.

--- src/MSDNToAnkiApp/Program.fs
+++ src/MSDNToAnkiApp/Program.fs
@@ -3,13 +3,15 @@ open HtmlAgilityPack
 
 open ExtractorHelpers
 
+type Card = {Front:string; Back:string; Image:string}
+
 let parseValue url =
   let web = new HtmlWeb()
   let doc = web.Load(url)
   let name = htmlDecode (doc.DocumentNode.SelectSingleNode("//h1[@class='title']").InnerText.Trim())
   let description = htmlDecode (doc.DocumentNode.SelectSingleNode("//div[@class='introduction']/p[1]").InnerText.Trim())
   
-  ()
+  {Front=name; Back=description; Image=""}
   //todo: Extract the screenshot of any example code
   //todo: Export these to cards
 
@@ -19,7 +21,7 @@ let parseTypeAbbreviation url =
   let name = htmlDecode (doc.DocumentNode.SelectSingleNode("//h1[@class='title']").InnerText.Trim())
   let description = htmlDecode (doc.DocumentNode.SelectSingleNode("//div[@class='introduction']/p[1]").InnerText.Trim())
   
-  ()
+  {Front=name; Back=description; Image=""}
   //todo: Extract the screenshot above the remark.
   //todo: export type abbrevations to cards.
 
@@ -29,17 +31,19 @@ let parseType url =
   let name = htmlDecode (doc.DocumentNode.SelectSingleNode("//h1[@class='title']").InnerText.Trim())
   let description = htmlDecode (doc.DocumentNode.SelectSingleNode("//div[@class='introduction']/p[1]").InnerText.Trim())
   let headings = selectNodes doc.DocumentNode "//h2[@class='LW_CollapsibleArea_TitleDiv']"
+ 
+  let compactTable data = Seq.fold (fun acum (mem, desc) -> acum + "<b>"+mem+"</b>"+" -> "+desc+"<br>") "" data
+  let instanceMembers = extractTableInfo2Column headings "Instance Members"  |> compactTable
+  let staticMembers = extractTableInfo2Column headings "Static Members" |> compactTable
+  let unionCases = extractTableInfo2Column headings "Union Cases" |> compactTable
   
-  let instanceMembers = extractTableInfo2Column headings "Instance Members"
-  let staticMembers = extractTableInfo2Column headings "Static Members"
-  let unionCases = extractTableInfo2Column headings "Union Cases"
-  
-  ()
-  //todo: Export these to cards
+  [{Front="Instance members of " + name; Back=instanceMembers; Image=""};
+   {Front="Static members of " + name; Back=staticMembers; Image=""};
+   {Front="Unions cases of " + name; Back=unionCases; Image=""}]
 
 let rec parseModule url =
   let headings = getHeadings url
-  extractTableInfo headings "Values" |> Seq.iter parseValue
+  extractTableInfo headings "Values" |> Seq.map parseValue
   extractTableInfo headings "Modules" |> Seq.iter parseModule
   //todo: Extract screenshot of any example code (see https://msdn.microsoft.com/en-us/library/ee353880.aspx)
 
@@ -47,8 +51,8 @@ let rec parseNamespace url =
   let headings = getHeadings url
   
   extractTableInfo headings "Modules" |> Seq.iter parseModule
-  extractTableInfo headings "Type Definitions" |> Seq.iter parseType
-  extractTableInfo headings "Type Abbreviations" |> Seq.iter parseTypeAbbreviation
+  extractTableInfo headings "Type Definitions" |> Seq.map parseType
+  extractTableInfo headings "Type Abbreviations" |> Seq.map parseTypeAbbreviation
   extractTableInfo headings "Namespaces" |> Seq.iter parseNamespace
 
 let parseLibrary (url:string) =
